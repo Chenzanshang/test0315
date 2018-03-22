fb59a76e64d37750b8022534923afa43cd846504
Chenzanshang <d04220282@gm.tut.edu.tw>
2018-03-22 15:40:53 +0800

    上課作業

--- 0322/t1.php
+++ 0322/t1.php
@@ -1,10 +1,10 @@
 <?php
-echo "^_^";
+
 $a=$_GET['aaa'];
 $b=$_GET['bbb'];
 $c=$a+$b;
 
-if( !isset($_GET['aaa'])||!isset($_GET['aaa']) ){
+if( !isset($_GET['aaa'])||!isset($_GET['bbb']) ){
 	echo"乖乖設定ab";
 }else{
 	echo $c;
