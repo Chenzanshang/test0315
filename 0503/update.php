14c5bc7a806d8ac1c8fdaec6018f7043e9c3d3ba
Chenzanshang <d04220282@gm.tut.edu.tw>
2018-05-31 16:51:40 +0800

    0531

--- 0503/update.php
+++ 0503/update.php
@@ -1,7 +1,5 @@
 <?php
 //輸入=======================================================
-
-
 //預期收到的資料格式  { prod:"商品的名稱", price:"值 }
 $data = json_decode(file_get_contents('php://input'));
 //檢查你的資料. 暫時省略
@@ -18,11 +16,10 @@ try {
 	//echo $err->getMessage(); //測試的時候用
 	exit;
 }
-$stmt = $db->prepare('delete from  moneybook where m_id');
-$stmt->execute( [ $data->{"id"} ] );
+$stmt = $db->prepare('update moneybook set name=?,cost=? where m_id=?');
+$stmt->execute( [ $data->{"prod"}, $data->{"price"}, $data->{"id"} ] );
 //輸出=======================================================
-//$data->{"id"} = $db->lastInsertId();  //取得前一次 insert 的 id
-
+//不需要$data->{"id"} = $db->lastInsertId();  //取得前一次 insert 的 id
 http_response_code(200);
 header("Content-Type: application/json;charset=UTF-8");
 echo json_encode($data);              //把insert的資料再回傳給用戶端
